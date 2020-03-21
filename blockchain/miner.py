933c1eac157886c2c0cb914b51258b096caaed1d
LD <LDinho@users.noreply.github.com>
2020-03-21 10:08:41 -0700

    add coin mining

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,6 +25,11 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    encoded_last_proof = f'{last_proof}'.encode()
+    last_hash = hashlib.sha256(encoded_last_proof).hexdigest()
+
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +45,10 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
