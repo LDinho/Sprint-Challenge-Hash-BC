ad2a59ac882deb4b2f58139e50edf8cfff696505
LD <LDinho@users.noreply.github.com>
2020-03-21 11:11:35 -0700

    complete get_indices method

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,17 +1,26 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)  # insert weights into hashtable
+
+    for i in range(length):
+        index_value = hash_table_retrieve(ht, limit - weights[i])  # limit - indexed weights
+
+        print(f'value is {index_value}')
+
+        if index_value is not None:
+            if i > index_value:
+                return i, index_value
+            else:
+                # return value first if index not larger
+                return index_value, i
 
     return None
 
